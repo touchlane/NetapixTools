bbdf93c976c0a35b2735f10f47c47f2670a438a8
Evgeny Dedovets <ededovets@touchlane.com>
2018-07-20 19:23:18 +0300

    loss graph script added

---
+++ README.md
@@ -0,0 +1,9 @@
+How to run.
+
+* Install python3 via homebrew.
+```brew install python```  
+* Install dependencies.
+```pip3 install -r requirements.txt```
+* Put log output in input.txt file.
+* Run the script.
+```python3 ./loss_graph/main.py input.txt```
---
+++ loss_graph/source/main.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import re
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    if len(sys.argv) < 2:
+        print("Enter path to the log file.")
+        sys.exit()
+
+    input_filepath = os.path.abspath(sys.argv[1])
+    if not os.path.isfile(input_filepath):
+        print("File path {} does not exist.".format(input_filepath))
+        sys.exit()
+
+    lines = [line.rstrip('\n') for line in open(input_filepath)]
+    indices = []
+    values = []
+    indices_valid = []
+    values_valid = []
+    for line in lines:
+        error_res = re.findall(r'Iteration: (\d+).*Error: (\d+\.\d+)', line)
+        valid_res = re.findall(r'Epoch: (\d+).*Iteration: (\d+).*Cross Validation Error: (\d+\.\d+)', line)
+        if error_res:
+            index, value = error_res[0]
+            indices.append(int(index))
+            values.append(float(value))
+        if valid_res:
+            epoch, index, value = valid_res[0]
+            indices_valid.append(int(index))
+            values_valid.append(float(value))
+
+    plt.plot(indices, values, '-', indices_valid, values_valid, '-')
+#plt.plot(indices_valid, values_valid)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
