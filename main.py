dc83334a9713af18be49afb3855be66957018b28
marvel31415 <marvel9898@gmail.com>
2018-08-02 04:10:17 +0300

    script modified

--- main.py
+++ main.py
@@ -1,44 +1,36 @@
 from __future__ import division
-import imageio
-from array import array
+import skimage.io as io
 import os
 import sys
 import struct
-import psutil._psutil_osx
+import glob
 
 
-def make_output_file_url(jpg_file_url):
-    output_file_name = os.path.basename(jpg_file_url).split('.')[0]
-    output_file_url = os.getcwd() + '/'
-    if not os.path.exists(output_file_url + "output"):
-        os.makedirs(output_file_url + "output")
-    return output_file_url + "output/" + output_file_name + ".npt"
-
-
-def make_output_file(jpg_filepath, txt_filepath):
-    image = imageio.imread(jpg_filepath)
-    output_file_url = make_output_file_url(jpg_filepath)
-    f = open(output_file_url, "wb+")
-    is_gray = 0
-    result = []
-    if len(image.shape) < 3:
-        is_gray = 1
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            if not is_gray:
-                r, g, b = tuple(image[i][j])
-                result.extend([r / 255, g / 255, b / 255])
-            else:
-                gray = image[i][j]
-                result.append(gray / 255)
-    with open(txt_filepath) as file:
-        for line in file:
-            for num in line.strip().split(' '):
-                result.append(float(num))
-    file.close()
-    f.write(struct.pack('%sf' % len(result), *result))
-    del result
-    f.close()
+def make_output_file(jpg_filepath, txt_filepath, output_file):
+    if os.path.isfile(txt_filepath):
+        image = io.imread(jpg_filepath)
+        f = open(output_file, "wb+")
+        is_gray = 0
+        result = []
+        if len(image.shape) < 3:
+            is_gray = 1
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                if not is_gray:
+                    r, g, b = tuple(image[i][j])
+                    result.extend([r / 255, g / 255, b / 255])
+                else:
+                    gray = image[i][j]
+                    result.append(gray / 255)
+        del image
+        with open(txt_filepath) as  my_file:
+            for line in my_file:
+                for num in line.strip().split(' '):
+                    result.append(float(num))
+        my_file.close()
+        f.write(struct.pack('%sf' % len(result), *result))
+        del result
+        f.close()
 
 
 def jpg_and_txt_to_npt_file():
@@ -66,25 +58,26 @@ def jpg_and_txt_to_npt_file():
     make_output_file(input_jpg_filepath, input_txt_filepath)
 
 
-def jpg_and_txt_to_npt_folder():
+def jpg_and_txt_to_npt_folder(output_folder):
     if len(sys.argv) < 3:
         print("Enter paths to log folders")
         sys.exit()
-    for subdir, dirs, first_files in os.walk(sys.argv[1]):
-        for subdir, dirs, second_files in os.walk(sys.argv[2]):
-            for my_filename1 in first_files:
-                for my_filename2 in second_files:
-                    if my_filename1.split(".")[0] == my_filename2.split(".")[0]:
-                        print(psutil.virtual_memory())
-                        # my_filename1 = os.path.abspath(my_filename1).split(my_filename1)[0] + sys.argv[1] + "/" + my_filename1
-                        my_filename1 = os.path.abspath(sys.argv[1]) +'/' + my_filename1
-                        my_filename2 = os.path.abspath(sys.argv[2]) +'/' + my_filename2
-                        # my_filename2 = os.path.abspath(my_filename2).split(my_filename2)[0] + sys.argv[2] + "/" + my_filename2
-                        make_output_file(my_filename1, my_filename2) if (".jpg" in my_filename1) else make_output_file(my_filename2, my_filename1)
+    second_path = os.path.abspath(sys.argv[2]) + '/'
+    first_files = glob.glob(sys.argv[1] + '/*.jpg')
+    for my_filename1 in first_files:
+        buf = my_filename1.split('/')[-1].split('.')[0]
+        my_filename2 = second_path + buf + ".txt"
+        output_file = output_folder + '/' + buf + ".npt"
+        make_output_file(my_filename1, my_filename2, output_file) if (".jpg" in my_filename1) else make_output_file(my_filename2, my_filename1, output_file)
+        del buf
+        del my_filename2
 
 
 if __name__ == '__main__':
+    output_folder = os.getcwd() + "/output"
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
     if os.path.isfile(os.path.abspath(sys.argv[1])) and os.path.isfile(os.path.abspath(sys.argv[2])):
         jpg_and_txt_to_npt_file()
     else:
-        jpg_and_txt_to_npt_folder()
+        jpg_and_txt_to_npt_folder(output_folder)
